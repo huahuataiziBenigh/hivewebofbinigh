bdf5747de5c50a02f8fac943cecd4527cd0e0550
huahuataiziBenigh <45790477+huahuataiziBenigh@users.noreply.github.com>
2021-12-24 14:08:53 +0800

    Add files via upload

--- connect_hive.py
+++ connect_hive.py
@@ -4,7 +4,7 @@ def connect_hive():  # 此处函数名，任意命名都行。
     # ali yun
     # conn = connect(host='39.105.27.82', port=10000, auth_mechanism='PLAIN', user='root', password='root')
     # tx yun
-    conn = connect(host='118.195.146.34', port=10000, auth_mechanism='PLAIN', user='root', password='root')
+    conn = connect(host='119.45.131.150', port=10000, auth_mechanism='PLAIN', user='root', password='root')
     # impala = connect('116.168.1.20', port=11050, user='hdxdf')  # 连接方式
     print("connect to hive success")
     return conn  # 返回变量impala
